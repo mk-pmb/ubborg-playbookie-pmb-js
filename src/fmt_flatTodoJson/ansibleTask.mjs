ea587d551a2a4c4ac4271b529787c8c7dc32d85f
mk-pmb <mk-pmb@users.noreply.github.com>
2020-07-28 02:13:36 +0200

    ansibleTask: Support blocks

--- src/fmt_flatTodoJson/ansibleTask.mjs
+++ src/fmt_flatTodoJson/ansibleTask.mjs
@@ -1,8 +1,12 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
 function translate(ctx) {
-  const { popProp } = ctx;
-  return popProp('task');
+  const mustPop = ctx.popProp.mustBe;
+  const tasks = [].concat(mustPop('undef | dictObj', 'task'),
+    mustPop('undef | ary', 'tasks')).filter(Boolean);
+  const blockExtras = mustPop('undef | dictObj', 'blockExtras');
+  if (!blockExtras) { return tasks; }
+  return { ...blockExtras, block: tasks };
 }
 
 export default translate;
