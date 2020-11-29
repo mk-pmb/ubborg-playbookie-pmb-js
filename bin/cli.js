cc65989e647baeb7ea5e60db28ecfe5a67f6a6a3
mk-pmb <mk-pmb@users.noreply.github.com>
2020-11-29 18:17:52 +0100

    v0.0.31: Fix CLI

---
+++ bin/cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+// eslint-disable-next-line spaced-comment
+/*jslint node: true */
+require('esmod-pmb')(module);
--- bin/cli.mjs
+++ bin/cli.mjs
@@ -1,4 +1,3 @@
-#!/usr/bin/env nodemjs
 // -*- coding: utf-8, tab-width: 2 -*-
 
 import 'usnam-pmb';
