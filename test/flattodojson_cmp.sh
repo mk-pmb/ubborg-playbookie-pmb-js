803d86afbf94ffed6453ad5a2d429c42c1d7bd4b
mk-pmb <mk-pmb@users.noreply.github.com>
2020-12-14 21:35:04 +0100

    v0.0.33: Add utility for diff-testing

---
+++ test/flattodojson_cmp.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+
+
+function pbk_cmp () {
+  export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
+  local SELFPATH="$(readlink -m -- "$BASH_SOURCE"/..)"
+  local ITEM= UNIQ=
+  printf -v UNIQ '@%(%y%m%d-%H%M%S)T.%s' -1 "$$"
+
+  local TODO=( "$@" )
+  case "$#:$1" in
+    1:--selfscan )
+      cd -- "$SELFPATH" || return $?
+      TODO=( */*.pbk-want.yaml )
+      ;;
+  esac
+  for ITEM in "${TODO[@]}"; do
+    [ -f "$ITEM" ] || return $?$(echo "E: no such file: $ITEM" >&2)
+    ITEM="${ITEM%.mjs}"
+    ITEM="${ITEM%.pbk-want.yaml}"
+    pbk_cmp_one "$ITEM" || return $?
+  done
+}
+
+
+function pbk_cmp_one () {
+  local PLAN="$1"
+  echo -n "$PLAN: "
+  local BFN="$(basename -- "$PLAN")"
+  local DEST="tmp.pbk.$BFN.$UNIQ.yaml"
+  local DIFF="tmp.pbk.$BFN.diff"
+  SECONDS=0
+  ubborg-planner-pmb depsTree --format=flatTodoJson "$PLAN" \
+    | ubborg-playbookie-pmb '<fmt=json>/dev/stdin' >"$DEST"
+  echo -n "$SECONDS sec, "
+  wc --lines -- "$DEST"
+
+  local WANT="$PLAN.pbk-want.yaml"
+  if [ -s "$WANT" ]; then
+    if diff -sU 5 -- "$WANT" "$DEST" >"$DIFF"; then
+      echo "    == $WANT"
+      rm -- "$DIFF" "$DEST"
+    else
+      head --lines=20 -- "$DIFF" | colordiff
+    fi
+  fi
+}
+
+
+pbk_cmp "$@"; exit $?
